a9b410a3e42ad099966ca0b6b6347c06d51280a6
CezaryZal <czarek0821@gmail.com>
2019-12-17 23:57:31 +0100

    Add Swagger UI

--- src/main/java/com/CezaryZal/entity/User.java
+++ src/main/java/com/CezaryZal/entity/User.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
@@ -21,4 +24,19 @@ public class User {
     private String permissions;
 
     private Long userId;
+
+
+    public List<String> getRoleList(){
+        if(this.roles.length() > 0){
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    public List<String> getPermissionList(){
+        if(this.permissions.length() > 0){
+            return Arrays.asList(this.permissions.split(","));
+        }
+        return new ArrayList<>();
+    }
 }
