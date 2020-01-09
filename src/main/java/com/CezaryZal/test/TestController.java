03d668daafcc8c347027bedd0568efb9ffa3419f
CezaryZal <czarek0821@gmail.com>
2020-01-09 13:18:24 +0100

    Add endpoint in TestController.java
    POST method connection with HealthCalendar application;

--- src/main/java/com/CezaryZal/test/TestController.java
+++ src/main/java/com/CezaryZal/test/TestController.java
@@ -1,6 +1,7 @@
 package com.CezaryZal.test;
 
 import com.CezaryZal.entity.health.calendar.UserCreator;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,12 +33,26 @@ public class TestController {
     public ResponseEntity<UserCreator> getUserCreatorFromHcApplication(){
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<UserCreator> responseEntity = restTemplate.exchange("http://localhost:8081/HealthCalendar/test/user",
+        ResponseEntity<UserCreator> responseEntity = restTemplate.exchange(
+                "http://localhost:8081/HealthCalendar/test/user",
                 HttpMethod.GET,
                 null,
                 UserCreator.class);
 
-        Stream.of(responseEntity.getBody()).forEach(System.out::println);
+        return responseEntity;
+    }
+
+    @GetMapping("/connection/login")
+    public ResponseEntity<String> getLoginFromHc(){
+        UserCreator tmpUser = new UserCreator("Dzielny", "dziel@hh.com");
+        HttpEntity<UserCreator> httpEntity = new HttpEntity<>(tmpUser);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                "http://localhost:8081/HealthCalendar/test/get/login",
+                HttpMethod.POST,
+                httpEntity,
+                String.class);
 
         return responseEntity;
     }
